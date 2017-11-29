1100c06240f21eacd2a6868e30e05038a3136207
molnartimi <molnar.timi7@gmail.com>
2017-11-29 02:43:24 +0100

    Thesis becomes nicer, tables, figures

--- BayesianOptimization/test/shogunopt_test.py
+++ BayesianOptimization/test/shogunopt_test.py
@@ -9,19 +9,19 @@ class ShogunOptTest(unittest.TestCase):
     #@unittest.skip("works fine")
     def test_simple_server_opt(self):
         shogun = MyShogunOpt(models.simple_server)
-        result = shogun.optimize(10,10)
+        result = shogun.optimize(20,20)
         result.print_result()
 
-    @unittest.skip("long")
+    #@unittest.skip("long")
     def test_vcl_stochastic_opt(self):
         shogun = MyShogunOpt(models.vcl_stochastic)
-        result = shogun.optimize(2,100)
+        result = shogun.optimize(20,20)
         print(result)
 
-    @unittest.skip("too long")
+    #@unittest.skip("too long")
     def test_hybrid_cloud_opt(self):
         shogun = MyShogunOpt(models.hybrid_cloud)
-        result = shogun.optimize(5,20)
+        result = shogun.optimize(20,20)
         print(result)
 
 if __name__ == '__main__':
